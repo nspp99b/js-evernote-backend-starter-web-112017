7d9e7c6f727623f5690e708bf727f33cff0b0d74
walkingalchemy <walkingalchemy@gmail.com>
2018-01-17 14:14:28 -0500

    Add extra user with notes to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-u = User.create(name: ENV['USER'])
+us = User.create(name: ENV['USER'])
+person = User.create(name: ENV['USER'])
 
 Note.destroy_all
 10.times do
-  Note.create(user: u, title: Faker::Lorem.sentence(rand(4) + 1, true), body: Faker::Lorem.paragraphs(3, true).join('\n'))
+  Note.create(user: us, title: Faker::Lorem.sentence(rand(4) + 1, true), body: Faker::Lorem.paragraphs(3, true).join('\n'))
+end
+10.times do
+  Note.create(user: person, title: Faker::Lorem.sentence(rand(4) + 1, true), body: Faker::Lorem.paragraphs(3, true).join('\n'))
 end
